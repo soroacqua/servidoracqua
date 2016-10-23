c3e7d53f427b4fdfedfe8b59689370efdad52b6b
Michel Paiva <Michel Paiva>
2016-10-23 16:32:45 -0200

    Documentos senha alterada

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-mongoose.connect('mongodb://teste:teste@ds015924.mlab.com:15924/soroacqua');
+mongoose.connect('mongodb://localhost');
 
 
 var app = express();
